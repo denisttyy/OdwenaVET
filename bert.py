7ea12e95bd51baa15bbf17d6f723820e96a4bca5
denisttyy <104391424+denisttyy@users.noreply.github.com>
2023-02-16 16:14:00 +0300

    Update bert.py

--- bert.py
+++ bert.py
@@ -5,7 +5,6 @@ model = BertModel.from_pretrained("bert-base-uncased")
 text = "Replace me by any text you'd like."
 
 
-def bert_embeddings(text):
     # text = "Replace me by any text you'd like."
     encoded_input = tokenizer(text, return_tensors="pt")
     output = model(**encoded_input)
@@ -16,12 +15,10 @@ from transformers import RobertaTokenizer, RobertaModel
 
 tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 model = RobertaModel.from_pretrained("roberta-base")
-text = "Replace me by any text you'd like."
 
 
 def Roberta_embeddings(text):
     # text = "Replace me by any text you'd like."
-    encoded_input = tokenizer(text, return_tensors="pt")
     output = model(**encoded_input)
     return output
 
